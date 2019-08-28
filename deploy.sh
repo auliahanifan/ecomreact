82e68b8d0cf5915934c77f67aa68b8942e6c40c0
Aulia <ahanifan@alterra.id>
2019-08-28 22:08:56 +0700

    yes

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo docker stop backend
-sudo docker rm backend
-sudo docker rmi fikriamri/e-commerce-backend:v.1
-sudo docker run -d -p 500:500 --name backend fikriamri/e-commerce-backend:v.1
