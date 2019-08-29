3834edacd05b9bee8cafa53595828f390de96bd9
Aulia <ahanifan@alterra.id>
2019-08-29 22:18:46 +0700

    ok

--- src/components_admin/FooterAdmin.js
+++ src/components_admin/FooterAdmin.js
@@ -3,7 +3,7 @@ import React from "react";
 class FooterAdmin extends React.Component {
     render() {
         return (
-            <footer className="py-5" style={{ backgroundColor: "#15c6c0" }}>
+            <footer className="py-5" style={{ backgroundColor: "#000000" }}>
                 <div className="container">
                     <p className="m-0 text-center text-white">
                         Copyright &copy; 2019 Ba-ik.com | All Rights Reserved
