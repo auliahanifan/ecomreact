d184469c5f4fb772aeee86e44cb599f4b9a467f7
Aulia <ahanifan@alterra.id>
2019-08-29 22:35:27 +0700

    ceria

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker rm -f react
-sudo docker rmi -f ahanifan/react-frontend:latest
-sudo docker run -d -p 3000:80 --name react ahanifan/react-frontend:latest
+sudo docker rmi -f ahanifan/react-ceria:latest
+sudo docker run -d -p 3000:80 --name react ahanifan/react-ceria:latest
